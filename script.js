2133fe242b37c82c5d6ab5c284208a3eaf1aec3c
RomaArtur <arturromafurtado@gmail.com>
2025-10-23 13:32:26 -0300

    Alterações na lógica, adição de condicional para verificar preenchimentos

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-const apiKey = null;
+const apiKey = "c17065774883b5115ac515a1e6e75b28";
+
 const searchButton = document.getElementById("searchButton");
 const weatherResult = document.getElementById("weatherResult");
+const cityInput = document.getElementById("cityInput");
+
+if (!searchButton || !cityInput || !weatherResult) {
+    console.error("Erro ao buscar dados. Cidade não encontrada ou chave de API inválida.")
+}
+
+
 
 searchButton.addEventListener("click", (evento) => {
-  const cityInput = document.getElementById("cityInput");
-  console.log(cityInput);
+  const cityName = cityInput.value.trim();
 });
