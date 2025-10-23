b5b9379a8997cee70cd211c7ae809566c2605a48
RomaArtur <arturromafurtado@gmail.com>
2025-10-23 13:14:42 -0300

    Começando a elaborar a lógica do projeto

---
+++ script.js
@@ -0,0 +1,8 @@
+const apiKey = null;
+const searchButton = document.getElementById("searchButton");
+const weatherResult = document.getElementById("weatherResult");
+
+searchButton.addEventListener("click", (evento) => {
+  const cityInput = document.getElementById("cityInput");
+  console.log(cityInput);
+});
