5621fddce30b3d02caa5c52f0a25c9e2814d70e0
RomaArtur <arturromafurtado@gmail.com>
2025-10-24 17:08:13 -0300

    Adição do script no HTML

--- script.js
+++ script.js
@@ -5,52 +5,55 @@ const weatherResult = document.getElementById("weatherResult");
 const cityInput = document.getElementById("cityInput");
 
 if (!searchButton || !cityInput || !weatherResult) {
-    console.error("Erro ao buscar dados. Cidade não encontrada ou chave de API inválida.")
+  console.error(
+    "Erro ao buscar dados. Cidade não encontrada ou chave de API inválida."
+  );
 }
 
 searchButton.addEventListener("click", () => {
-  const cityName = cityInput.value.trim(); 
+  const cityName = cityInput.value.trim();
 
   if (cityName) {
     getWeatherData(cityName);
   }
-})
-  function getWeatherData(city) {
-    weatherResult.innerHTML = "<p>Buscando...</p>";
+});
+function getWeatherData(city) {
+  weatherResult.innerHTML = "<p>Buscando...</p>";
 
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=pt_br`;
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=pt_br`;
 
-    fetch(apiUrl)
-      .then(response => {
-        if (!response.ok) {
-        throw new Error("Erro ao buscar dados. Cidade não encontrada ou chave API inválida.");
+  fetch(apiUrl)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(
+          "Erro ao buscar dados. Cidade não encontrada ou chave API inválida."
+        );
       }
       return response.json();
-    }) 
-    .then(data => {
+    })
+    .then((data) => {
       displayWeather(data);
     })
-    .catch(error => {
+    .catch((error) => {
       console.error("Houve um erro de requisição:", error);
-      weatherResult.innerHTML = `<p style="color: red;">Erro: ${error.message}</p>`
+      weatherResult.innerHTML = `<p style="color: red;">Erro: ${error.message}</p>`;
     });
-  }
+}
 
-  function displayWeather(data) {
+function displayWeather(data) {
+  const temp = data.main.temp;
+  const description = data.weather[0].description;
+  const city = data.name;
+  const country = data.sys.country;
 
-    const temp = data.main.temp;
-    const description = data.weather[0].description;
-    const city = data.name;
-    const country = data.sys.country;
+  const formattedDescription =
+    description.charAt(0).toUpperCase() + description.slice(1);
 
-    const formattedDescription = description.charAt(0).toUpperCase() + description.slice(1);
-
-    const htmlContent = `
+  const htmlContent = `
       <h2>${city}, ${country}</h2>
       <p>Temperatura: ${temp.toFixed(1)} graus Celcius</p>
       <p>Condição: ${formattedDescription}</p> 
     `;
 
-    weatherResult.innerHTML = htmlContent;
-  }
-
+  weatherResult.innerHTML = htmlContent;
+}
